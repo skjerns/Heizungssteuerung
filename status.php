e1b4ca6e28474141651ee72e8ce62f7d6e629983
Simon Kern <14980558+skjerns@users.noreply.github.com>
2026-01-11 16:57:51 +0100

    also return history

--- status.php
+++ status.php
@@ -3,26 +3,25 @@ header('Content-Type: application/json');
 
 $config = json_decode(file_get_contents('config.json'), true);
 
-$roomTempFile = $config['room_temp_file'];
-$eq3TempFile = $config['eq3_temp_file'];
+$roomTempFile = $config['room_temperature_csv'];
+$eq3TempFile = $config['eq3_temperature_csv'];
+$nLines = 2000; // Set desired number of history lines
 
-function getLastLine($filename) {
+function getLastLines($filename, $n) {
+    if (!file_exists($filename)) return null;
     $data = file($filename);
-    return trim(end($data));
+    return array_map('trim', array_slice($data, -$n));
 }
 
-$roomTemp = null;
-if (file_exists($roomTempFile)) {
-    $roomTemp = getLastLine($roomTempFile);
-}
+$room_history = getLastLines($roomTempFile, $nLines);
+$eq3_history = getLastLines($eq3TempFile, $nLines);
 
-$eq3Temp = null;
-if (file_exists($eq3TempFile)) {
-    $eq3Temp = getLastLine($eq3TempFile);
-}
+$roomTemp = !empty($room_history) ? end($room_history) : null;
+$eq3Temp = !empty($eq3_history) ? end($eq3_history) : null;
 
 echo json_encode([
     'room_temp' => $roomTemp,
     'eq3_temp' => $eq3Temp,
+    'room_hist' => $room_history,
+    'eq3_hist' => $eq3_history
 ]);
-?>
